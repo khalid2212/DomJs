34ba44c9ce54420129545d3aa5f22094d4559889
khalid2212 <48457888+khalid2212@users.noreply.github.com>
2023-10-22 20:15:42 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let mydiv = document.querySelector('div')
+            
+
+mydiv.onmouseover = function() {
+    document.body.style.backgroundColor = 'gray';
+}
+
+mydiv.onmouseout = function() {
+    document.body.style.backgroundColor = 'teal';
+}
